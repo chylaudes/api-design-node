09dc4d349b57a60f9646975903550a284082dd98
Christine Lau <chylau.design@gmail.com>
2016-09-08 12:11:11 -0700

    finished with step 4

--- server/lions.js
+++ server/lions.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var lionRouter = require('express').Router();
 
 var lions = [];
@@ -12,10 +13,10 @@ var updateId = function(req, res, next) {
 };
 
 lionRouter.param('id', function(req, res, next, id) {
-  var todo = _.find(todos, {id: id});
+  var lion = _.find(lions, {id: id});
 
-  if (todo) {
-    req.todo = todo;
+  if (lion) {
+    req.lion = lion;
     next();
   } else {
     res.send();
@@ -27,7 +28,7 @@ lionRouter.get('/', function(req, res){
 });
 
 lionRouter.get('/:id', function(req, res){
-  var lion = req.todo;
+  var lion = req.lion;
   res.json(lion || {});
 });
 
@@ -43,7 +44,7 @@ lionRouter.post('/', updateId, function(req, res) {
 lionRouter.put('/:id', function(req, res) {
   var update = req.body;
   if (update.id) {
-    delete update.id
+    delete update.id;
   }
 
   var lion = _.findIndex(lions, {id: req.params.id});
